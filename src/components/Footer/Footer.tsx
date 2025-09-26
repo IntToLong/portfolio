273bbe243fd71d8eafbad465590a229504a59390
Nataliia Liubynets <natalialiubynets@gmail.com>
2025-09-26 21:29:51 +0200

    feat(footer): add designer acknowledgment

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,10 +5,23 @@ export default function Footer() {
   return (
     <footer className="bg-primary-black text-primary-white flex items-center justify-between px-5 py-5 md:px-20 lg:px-[112px]">
       <Logo color="white" />
-      <p className="flex items-center gap-2">
-        <span className="text-lg">&#169;</span>{" "}
-        <span>{year} Personal Portfolio</span>
-      </p>
+      <div className="flex flex-col">
+        <p className="flex items-center gap-2">
+          <span className="text-lg">&#169;</span>{" "}
+          <span>{year} Personal Portfolio</span>
+        </p>
+        <span>
+          Design inspired by{" "}
+          <a
+            href="https://www.figma.com/@jhanvishah"
+            className="text-blue-500"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Jhanvi Shah
+          </a>
+        </span>
+      </div>
     </footer>
   );
 }
